a3ae4a6910d3dbab2f93d853759ae59a502ff75a
Eddie Gingco <gingcoeddie971@gmail.com>
2021-11-14 15:37:13 +0800

    Second Commit

---
+++ app/src/main/java/com/example/loginui/HomeActivity.java
@@ -0,0 +1,21 @@
+package com.example.loginui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class HomeActivity extends AppCompatActivity {
+
+    public TextView user_id;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        user_id= (TextView) findViewById(R.id.user_id);
+
+        user_id.setText(getIntent().getExtras().getString("USER_ID"));
+    }
+}
